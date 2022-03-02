e51087dcb61a4cca8f19eefa440ff62496d4173c
pravnviji <pravnviji@gmail.com>
2022-03-02 12:35:08 +0800

    UnitTestCase for MotorRacingService

--- src/app/feature/motor-racing.service.spec.ts
+++ src/app/feature/motor-racing.service.spec.ts
@@ -1,16 +1,188 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
-import { MotorRacingService } from './motor-racing.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+import { MotorRacingService, TDriverStanding, TRaceResult } from '.';
+import { HttpErrorResponse } from '@angular/common/http';
+
+const dummyDriverStanding = {
+  MRData: {
+    StandingsTable: {
+      StandingsLists: [
+        {
+          DriverStandings: [
+            {
+              position: '1',
+              positionText: '1',
+              Driver: {
+                driverId: 'hamilton',
+                permanentNumber: '44',
+                givenName: 'Lewis',
+                familyName: 'Hamilton',
+                dateOfBirth: '1985-01-07',
+                nationality: 'British',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
+
+const dummySeasonResult = {
+  MRData: {
+    xmlns: 'http://ergast.com/mrd/1.4',
+    series: 'f1',
+    url: 'http://ergast.com/api/f1/2008/results/1.json',
+    limit: '30',
+    offset: '0',
+    total: '18',
+    RaceTable: {
+      season: '2008',
+      position: '1',
+      Races: [
+        {
+          season: '2008',
+          round: '1',
+          raceName: 'Australian Grand Prix',
+          Circuit: {
+            circuitName: 'Albert Park Grand Prix Circuit',
+            Location: {
+              lat: '-37.8497',
+              long: '144.968',
+              locality: 'Melbourne',
+              country: 'Australia',
+            },
+          },
+          Results: [
+            {
+              number: '22',
+              position: '1',
+              positionText: '1',
+              points: '10',
+              Driver: {
+                driverId: 'hamilton',
+                permanentNumber: '44',
+                code: 'HAM',
+                url: 'http://en.wikipedia.org/wiki/Lewis_Hamilton',
+                givenName: 'Lewis',
+                familyName: 'Hamilton',
+                dateOfBirth: '1985-01-07',
+                nationality: 'British',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
 
 describe('MotorRacingService', () => {
-  let service: MotorRacingService;
+  let serviceTest: MotorRacingService;
+  let httpMock: HttpTestingController;
+  let injector: TestBed;
+  let baseUrl = 'https://ergast.com/api/f1/';
+  let seasonResult = '/results/1.json';
+  let championshipResult = '/driverStandings/1.json';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MotorRacingService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [MotorRacingService],
+    });
+    injector = getTestBed();
+    serviceTest = injector.get(MotorRacingService);
+    httpMock = injector.get(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpMock.verify();
+  });
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(serviceTest).toBeTruthy();
+  });
+
+  it('getDriverStanding() should call valid URL with season', () => {
+    const season = '2013';
+    serviceTest.getDriverStanding(season).subscribe();
+    const reqMock = httpMock.expectOne(
+      (req) =>
+        req.method === 'GET' &&
+        req.url === baseUrl + season + championshipResult
+    );
+    expect(reqMock.request.method).toBe('GET');
+    reqMock.flush(dummyDriverStanding);
+  });
+
+  it('getDriverStanding() should call handleError()', () => {
+    const season = '2013';
+    spyOn(serviceTest as any, 'handleError').and.callThrough();
+    const errorData: HttpErrorResponse = new HttpErrorResponse({
+      error: {},
+      status: 500,
+      url: baseUrl + season + championshipResult,
+      statusText: 'Bad Request',
+    });
+    serviceTest.getDriverStanding(season).subscribe(
+      () => fail('should error'),
+      () => {
+        expect(serviceTest.handleError).toHaveBeenCalled();
+      }
+    );
+    const reqMock = httpMock.expectOne(
+      (req) =>
+        req.method === 'GET' &&
+        req.url === baseUrl + season + championshipResult
+    );
+
+    reqMock.flush(errorData);
+  });
+
+  it('getSeasonResult() should call valid URL with season', () => {
+    const season = '2013';
+    const driverStanding: TDriverStanding = {
+      driver: 'test',
+      driverId: 'test',
+    };
+    serviceTest.getSeasonResult(season, driverStanding).subscribe();
+    const reqMock = httpMock.expectOne(
+      (req) =>
+        req.method === 'GET' && req.url === baseUrl + season + seasonResult
+    );
+    expect(reqMock.request.method).toBe('GET');
+    reqMock.flush(dummySeasonResult);
+  });
+
+  it('getSeasonResult() should call handleError()', () => {
+    const season = '2013';
+    const driverStanding: TDriverStanding = {
+      driver: 'test',
+      driverId: 'test',
+    };
+    spyOn(serviceTest as any, 'handleError').and.callThrough();
+    const errorData: HttpErrorResponse = new HttpErrorResponse({
+      error: {},
+      status: 500,
+      url: baseUrl + season + championshipResult,
+      statusText: 'Bad Request',
+    });
+    serviceTest.getDriverStanding(season).subscribe(
+      () => fail('should error'),
+      () => {
+        expect(serviceTest.handleError).toHaveBeenCalled();
+      }
+    );
+    const reqMock = httpMock.expectOne(
+      (req) =>
+        req.method === 'GET' &&
+        req.url === baseUrl + season + championshipResult
+    );
+
+    reqMock.flush(errorData);
   });
 });
--- src/app/feature/season-list/season-list.component.spec.ts
+++ src/app/feature/season-list/season-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/*import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SeasonListComponent } from './season-list.component';
 
@@ -22,4 +22,4 @@ describe('SeasonListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+});*/
